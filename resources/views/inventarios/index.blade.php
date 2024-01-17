fdde8c9e80a83c25c4bcef356b43763538cd5832
Rick1701 <ricardo.flores1701@alumnos.ubiobio.cl>
2024-01-17 04:34:38 -0300

    CRUD salas y bodegas

--- resources/views/inventarios/index.blade.php
+++ resources/views/inventarios/index.blade.php
@@ -40,6 +40,19 @@
                 <a class="btn btn-primary" href="{{ route('tiposmateriales.create') }}"><i class="fa-solid fa-plus"></i> Agregar Tipo de Material</a>
             </div>
         </div>
+
+        {{-- Gestión de salas o bodegas --}}
+        <div class="card text-bg-primary mb-3 mx-auto col-sm-12 col-md-6" style="max-width: 100%; text-align: justify;">
+            <div class="card-header">Gestión de Salas y Bodegas</div>
+            <div class="card-body">
+                <p class="card-text">En este módulo puedes gestionar las <strong>Salas y Bodegas</strong>. Agrega, edita o elimina salas de reuniones y almacenes según sea necesario.</p>
+            </div>
+            <div class="card-footer">
+                {{-- Ajusta las rutas según tu estructura --}}
+                <a class="btn btn-primary" href="{{ route('salasobodegas.index') }}"><i class="fa-solid fa-box"></i> Ver Salas y Bodegas</a>
+                <a class="btn btn-primary" href="{{ route('salasobodegas.create') }}"><i class="fa-solid fa-plus"></i> Agregar Sala o Bodega</a>
+            </div>
+        </div>
     </div>
 </div>
 @stop
