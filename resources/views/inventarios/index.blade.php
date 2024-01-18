f73780495e29e4268dc80014f29e42a01e534393
Jorgito <jorge.valdivia1901@alumnos.ubiobio.cl>
2024-01-18 17:02:25 -0300

    Ajuste visual inventario.

--- resources/views/inventarios/index.blade.php
+++ resources/views/inventarios/index.blade.php
@@ -15,13 +15,14 @@
 
     <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
 
+        <div class="row">
         {{-- Gestión de materiales --}}
         <div class="card text-bg-primary mb-3 mx-auto col-sm-12 col-md-6" style="max-width: 100%; text-align: justify;">
             <div class="card-header">Gestión de Materiales</div>
             <div class="card-body">
                 <p class="card-text">En este módulo puedes gestionar los <strong>Materiales</strong>. Agrega, edita o elimina materiales según sea necesario.</p>
             </div>
-            <div class="card-footer">
+            <div class="card-footer ">
                 {{-- Ajusta las rutas según tu estructura --}}
                 <a class="btn btn-primary" href="{{ route('materiales.index') }}"><i class="fa-solid fa-box"></i> Ver Materiales</a>
                 <a class="btn btn-primary" href="{{ route('materiales.create') }}"><i class="fa-solid fa-plus"></i> Agregar Material</a>
@@ -30,18 +31,20 @@
 
         {{-- Gestión de tipos de materiales --}}
         <div class="card text-bg-primary mb-3 mx-auto col-sm-12 col-md-6" style="max-width: 100%; text-align: justify;">
-            <div class="card-header">Gestión de Tipos de Materiales</div>
-            <div class="card-body">
-                <p class="card-text">En este módulo puedes gestionar los <strong>Tipos de Materiales</strong>. Agrega, edita o elimina tipos de materiales según sea necesario.</p>
-            </div>
-            <div class="card-footer">
-                {{-- Ajusta las rutas según tu estructura --}}
-                <a class="btn btn-primary" href="{{ route('tiposmateriales.index') }}"><i class="fa-solid fa-tag"></i> Ver Tipos de Materiales</a>
-                <a class="btn btn-primary" href="{{ route('tiposmateriales.create') }}"><i class="fa-solid fa-plus"></i> Agregar Tipo de Material</a>
+                <div class="card-header">Gestión de Tipos de Materiales</div>
+                <div class="card-body">
+                    <p class="card-text">En este módulo puedes gestionar los <strong>Tipos de Materiales</strong>. Agrega, edita o elimina tipos de materiales según sea necesario.</p>
+                </div>
+                <div class="card-footer">
+                    {{-- Ajusta las rutas según tu estructura --}}
+                    <a class="btn btn-primary" href="{{ route('tiposmateriales.index') }}"><i class="fa-solid fa-tag"></i> Ver Tipos de Materiales</a>
+                    <a class="btn btn-primary" href="{{ route('tiposmateriales.create') }}"><i class="fa-solid fa-plus"></i> Agregar Tipo de Material</a>
+                </div>
             </div>
         </div>
 
-                {{-- Gestión de equipos --}}
+        <div class="row">
+        {{-- Gestión de equipos --}}
         <div class="card text-bg-primary mb-3 mx-auto col-sm-12 col-md-6" style="max-width: 100%; text-align: justify;">
             <div class="card-header">Gestión de Equipos</div>
             <div class="card-body">
@@ -60,11 +63,14 @@
             <div class="card-body">
                 <p class="card-text">En este módulo puedes gestionar los <strong>Tipos de Equipos</strong>. Agrega, edita o elimina tipos de equipos según sea necesario.</p>
             </div>
-            <div class="card-footer">
-                {{-- Ajusta las rutas según tu estructura --}}
-                <a class="btn btn-primary" href="{{ route('tiposequipos.index') }}"><i class="fa-solid fa-tag"></i> Ver Tipos de Equipos</a>
-                <a class="btn btn-primary" href="{{ route('tiposequipos.create') }}"><i class="fa-solid fa-plus"></i> Agregar Tipo de Equipos</a>
+                <div class="card-footer">
+                    {{-- Ajusta las rutas según tu estructura --}}
+                    <a class="btn btn-primary" href="{{ route('tiposequipos.index') }}"><i class="fa-solid fa-tag"></i> Ver Tipos de Equipos</a>
+                    <a class="btn btn-primary" href="{{ route('tiposequipos.create') }}"><i class="fa-solid fa-plus"></i> Agregar Tipo de Equipos</a>
+                </div>
             </div>
+        </div>
+        <div class="row">
         {{-- Gestión de salas o bodegas --}}
         <div class="card text-bg-primary mb-3 mx-auto col-sm-12 col-md-6" style="max-width: 100%; text-align: justify;">
             <div class="card-header">Gestión de Salas y Bodegas</div>
@@ -90,6 +96,7 @@
                 <a class="btn btn-primary" href="{{ route('salasobodegas.create') }}"><i class="fa-solid fa-plus"></i> Agregar Sala o Bodega</a>
             </div>
         </div>
+    </div>
     </div>
 </div>
 @stop
