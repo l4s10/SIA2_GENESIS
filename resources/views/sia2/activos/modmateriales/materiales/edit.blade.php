b8a4f68e90bcd59055c2ba4bbf0d63d6cf870a80
Jorgito <jorge.valdivia1901@alumnos.ubiobio.cl>
2024-01-19 20:15:48 -0300

    Actualizacion de editar. Color estandar.

--- resources/views/sia2/activos/modmateriales/materiales/edit.blade.php
+++ resources/views/sia2/activos/modmateriales/materiales/edit.blade.php
@@ -102,7 +102,7 @@
                 <input type="hidden" name="OFICINA_ID" value="{{ $oficina->OFICINA_ID }}">
             </div>
             <a href="{{ route('materiales.index') }}" class="btn btn-secondary" tabindex="5"><i class="fa-solid fa-hand-point-left"></i> Cancelar</a>
-            <button type="submit" class="btn btn-primary" tabindex="4"><i class="fa-solid fa-floppy-disk"></i> Guardar cambios</button>
+            <button type="submit" class="btn guardar" tabindex="4"><i class="fa-solid fa-floppy-disk"></i> Guardar cambios</button>
         </form>
     </div>
 @stop
@@ -111,6 +111,12 @@
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <style>/* Estilos personalizados si es necesario */
+        .guardar {
+            background-color: #e6500a;
+            color: #fff;
+        }
+    </style>
 @stop
 
 @section('js')
