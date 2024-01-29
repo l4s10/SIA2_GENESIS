201359be99846c21e25aac24cea65c94582799a4
FranciscoTarroW11 <franciscomunoz142@gmail.com>
2024-01-29 12:01:58 -0300

    creacion migracion solicitud de salas

---
+++ database/migrations/2024_01_29_121838_create_solicitudes_salas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solicitudes_salas', function (Blueprint $table) {
+            $table->unsignedBigInteger('SOLICITUD_ID');
+            $table->unsignedBigInteger('SALA_ID');
+            $table->integer('SOLICITUD_SALA_ID_ASIGNADA')->nullable();
+            $table->timestamps();
+
+            // Foraneas
+            $table->foreign('SOLICITUD_ID')->references('SOLICITUD_ID')->on('solicitudes');
+            $table->foreign('SALA_ID')->references('SALA_ID')->on('salas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solicitudes_salas');
+    }
+};
