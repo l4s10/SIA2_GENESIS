f4dfda448d1bb8d589a365410beab8dd24f592ca
FranciscoTarroW11 <franciscomunoz142@gmail.com>
2024-01-29 11:50:46 -0300

    Creacion de migracion para solicitud de bodegas

---
+++ database/migrations/2024_01_29_121856_create_solicitudes_bodegas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solicitudes_bodegas', function (Blueprint $table) {
+            $table->unsignedBigInteger('SOLICITUD_ID');
+            $table->unsignedBigInteger('BODEGA_ID');
+            $table->integer('SOLICITUD_BODEGA_ID_ASINGADA')->nullable();
+            $table->timestamps();
+
+            //foraneas
+            $table->foreign('SOLICITUD_ID')->references('SOLICITUD_ID')->on('solicitudes');
+            $table->foreign('BODEGA_ID')->references('BODEGA_ID')->on('bodegas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solicitudes_bodegas');
+    }
+};
