a994e743765e81f3ec04c258c96da6d0df965c44
FranciscoTarroW11 <franciscomunoz142@gmail.com>
2024-01-29 10:58:46 -0300

    Creacion del archivo de rutas para las salas y bodegas

--- routes/Solicitudes/SolBodegasRoutes.php
+++ routes/Solicitudes/SolBodegasRoutes.php
@@ -4,4 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Solicitud\SolicitudBodegasController;
 
 // Rutas para solicitudes de bodegas
-Route::resource('solicitudes/bodegas', SolicitudBodegasController::class);
+// Prefix para las rutas de bodegas solicitudes/
+Route::prefix('solicitudes')->group(function () {
+    //Definir cada ruta de solicitud de bodegas con nombre "solicitudesbodegas.{nombre_ruta}"
+    //** Listar solicitudes */
+    Route::get('/bodegas', [SolicitudBodegasController::class, 'index'])->name('solicitudes.bodegas.index');
+
+    //** Crear solicitud */
+    Route::get('/bodegas/create', [SolicitudBodegasController::class, 'create'])->name('solicitudes.bodegas.create');
+
+    //** Guardar solicitud */
+    Route::post('/bodegas', [SolicitudBodegasController::class, 'store'])->name('solicitudes.bodegas.store');
+
+    //** Mostrar solicitud */
+    Route::get('/bodegas/{solicitud}', [SolicitudBodegasController::class, 'show'])->name('solicitudes.bodegas.show');
+
+    //** Editar solicitud */
+    Route::get('/bodegas/{solicitud}/edit', [SolicitudBodegasController::class, 'edit'])->name('solicitudes.bodegas.edit');
+
+    //** Actualizar solicitud */
+    Route::put('/bodegas/{solicitud}', [SolicitudBodegasController::class, 'update'])->name('solicitudes.bodegas.update');
+
+    //** Eliminar solicitud */
+    Route::delete('/bodegas/{solicitud}', [SolicitudBodegasController::class, 'destroy'])->name('solicitudes.bodegas.destroy');
+});
--- routes/Solicitudes/SolSalasRoutes.php
+++ routes/Solicitudes/SolSalasRoutes.php
@@ -4,5 +4,29 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Solicitud\SolicitudSalasController;
 
 // Rutas para solicitudes de salas
-Route::resource('solicitudes/salas', SolicitudSalasController::class);
+
+// Prefix para las rutas de salas solicitudes/
+Route::prefix('solicitudes')->group(function () {
+    //Definir cada ruta de solicitud de salas con nombre "solicitudesmateriales.{nombre_ruta}"
+    //** Listar solicitudes */
+    Route::get('/salas', [SolicitudSalasController::class, 'index'])->name('solicitudes.salas.index');
+
+    //** Crear solicitud */
+    Route::get('/salas/create', [SolicitudSalasController::class, 'create'])->name('solicitudes.salas.create');
+
+    //** Guardar solicitud */
+    Route::post('/salas', [SolicitudSalasController::class, 'store'])->name('solicitudes.salas.store');
+
+    //** Mostrar solicitud */
+    Route::get('/salas/{solicitud}', [SolicitudSalasController::class, 'show'])->name('solicitudes.salas.show');
+
+    //** Editar solicitud */
+    Route::get('/salas/{solicitud}/edit', [SolicitudSalasController::class, 'edit'])->name('solicitudes.salas.edit');
+
+    //** Actualizar solicitud */
+    Route::put('/salas/{solicitud}', [SolicitudSalasController::class, 'update'])->name('solicitudes.salas.update');
+
+    //** Eliminar solicitud */
+    Route::delete('/salas/{solicitud}', [SolicitudSalasController::class, 'destroy'])->name('solicitudes.salas.destroy');
+});
 
