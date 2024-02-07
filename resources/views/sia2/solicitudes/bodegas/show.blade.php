f33628c89cff01be9be6e902e02dced2909bd049
Francisco munoz <franciscomunoz142@gmail.com>
2024-02-07 15:53:53 -0300

    correcciones vista show sol_bodegas

--- resources/views/sia2/solicitudes/bodegas/show.blade.php
+++ resources/views/sia2/solicitudes/bodegas/show.blade.php
@@ -7,32 +7,32 @@
 @stop
 
 @section('content')
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-                Detalles de la Solicitud
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Motivo: {{ $solicitud->SOLICITUD_MOTIVO }}</h5>
-                <p class="card-text">Estado: {{ $solicitud->SOLICITUD_ESTADO }}</p>
-                <p class="card-text">Fecha y Hora de Inicio Solicitada: {{ $solicitud->SOLICITUD_FECHA_HORA_INICIO_SOLICITADA }}</p>
-                <p class="card-text">Fecha y Hora de Término Solicitada: {{ $solicitud->SOLICITUD_FECHA_HORA_TERMINO_SOLICITADA }}</p>
-                <h5 class="mt-4">Bodega Solicita</h5>
-                <table class="table table-bordered">
-                    <thead>
+    <div class="card">
+        <div class="card-header">
+            Detalles de la Solicitud
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Motivo: {{ $solicitud->SOLICITUD_MOTIVO }}</h5>
+            <p class="card-text">Estado: {{ $solicitud->SOLICITUD_ESTADO }}</p>
+            <p class="card-text">Fecha y Hora de Inicio Solicitada: {{ $solicitud->SOLICITUD_FECHA_HORA_INICIO_SOLICITADA }}</p>
+            <p class="card-text">Fecha y Hora de Término Solicitada: {{ $solicitud->SOLICITUD_FECHA_HORA_TERMINO_SOLICITADA }}</p>
+            <h5 class="mt-4">Bodega Solicita</h5>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Nombre bodega</th>
+                        <th>Bodega asignada</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($solicitud->bodegas as $bodega)
                         <tr>
-                            <th>Nombre bodega</th>
+                            <td>{{ $bodega->BODEGA_NOMBRE }}</td>
+                            <td>{{ $bodegaAsignada->BODEGA_NOMBRE ?? 'SIN ASIGNACION.'}}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($solicitud->bodegas as $bodega)
-                            <tr>
-                                <td>{{ $bodega->BODEGA_NOMBRE }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @stop
