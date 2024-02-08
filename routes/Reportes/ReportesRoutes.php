7f47041e7144b1b31749c6070f30d7d4c769c671
Francisco munoz <franciscomunoz142@gmail.com>
2024-02-08 16:40:58 -0300

    optimizacion  de rutas

--- routes/Reportes/ReportesRoutes.php
+++ routes/Reportes/ReportesRoutes.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Reportes\ReportesController;
 
-Route::get('reportes/home',[ReportesController::class, 'Home'])->name('reportes.home.index');
-
-Route::get('reportes/materiales', [ReportesController::class, 'Materiales'])->name('reportes.materiales.index');
+Route::prefix('reportes')->group(function () {
+    Route::get('/home', [ReportesController::class, 'Home'])->name('reportes.home.index');
+    Route::get('/materiales', [ReportesController::class, 'Materiales'])->name('reportes.materiales.index');
+});
