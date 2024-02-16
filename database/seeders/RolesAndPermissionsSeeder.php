29677aefea2485bf66ac44b566aa94aba7982647
Francisco munoz <franciscomunoz142@gmail.com>
2024-02-16 16:19:40 -0300

    modifica codigo seeder para roles y permisos

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -9,28 +9,37 @@ use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //*Roles*/
-        $ADMINISTRADOR = Role::create(['name' => 'ADMINISTRADOR']);
-        $SERVICIOS = Role::create(['name' => 'SERVICIOS']);
-        $INFORMATICA = Role::create(['name' => 'INFORMATICA']);
-        $JURIDICO = Role::create(['name' => 'JURIDICO']);
-        $FUNCIONARIO = Role::create(['name' => 'FUNCIONARIO']);
+        // Limpiar caché de permisos
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        //*Permisos*/
-        $Nivel_1 = Permission::create(['name' => 'Nivel 1']);
-        $Nivel_2 = Permission::create(['name' => 'Nivel 2']);
-        $Nivel_3 = Permission::create(['name' => 'Nivel 3']);
+        // Crear roles
+        $roles = [
+            'ADMINISTRADOR' => Role::create(['name' => 'ADMINISTRADOR']),
+            'SERVICIOS' => Role::create(['name' => 'SERVICIOS']),
+            'INFORMATICA' => Role::create(['name' => 'INFORMATICA']),
+            'FUNCIONARIO' => Role::create(['name' => 'FUNCIONARIO']),
+        ];
 
-        //*Asignacion de permisos*/
-        $ADMINISTRADOR->syncPermissions([$Nivel_1, $Nivel_2, $Nivel_3]);
-        $SERVICIOS->syncPermissions([$Nivel_1, $Nivel_2]);
-        $INFORMATICA->syncPermissions([$Nivel_1, $Nivel_2]);
-        $JURIDICO->syncPermissions([$Nivel_1]);
-        $FUNCIONARIO->syncPermissions([$Nivel_1]);
+        // Definir permisos
+        $permisos = [
+            'crear_solicitud',
+            'ver_solicitudes', // Permiso único para ver el detalle y la lista de solicitudes
+            'editar_solicitud',
+            'actualizar_solicitud',
+            'eliminar_solicitud',
+        ];
+
+        // Crear permisos
+        foreach ($permisos as $permiso) {
+            Permission::create(['name' => $permiso]);
+        }
+
+        // Asignación de permisos simplificada
+        $roles['FUNCIONARIO']->givePermissionTo(['crear_solicitud', 'ver_solicitudes']);
+        $roles['SERVICIOS']->givePermissionTo(['crear_solicitud', 'ver_solicitudes', 'editar_solicitud', 'actualizar_solicitud']);
+        $roles['INFORMATICA']->givePermissionTo(['crear_solicitud', 'ver_solicitudes', 'editar_solicitud', 'actualizar_solicitud']);
+        $roles['ADMINISTRADOR']->givePermissionTo(Permission::all());
     }
 }
