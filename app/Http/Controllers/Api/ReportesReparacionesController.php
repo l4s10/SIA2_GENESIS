e1e69033168f875981b65dbc96591e52cc8629e4
FranciscoTarroW11 <franciscomunoz142@gmail.com>
2024-02-22 19:07:50 -0300

    avance graficos

--- app/Http/Controllers/Api/ReportesReparacionesController.php
+++ app/Http/Controllers/Api/ReportesReparacionesController.php
@@ -25,13 +25,14 @@ class ReportesReparacionesController extends Controller
         try{
             // Cargar los datos de los graficos
             $grafico1 = $this->reparacionesPorCategoria(new Request());
+            $grafico2 = $this->Grafico2(new Request());
 
             // Devolver los datos de los graficos
             return response()->json([
                 'status' => 'success',
                 'data' => [
                     'grafico1' => $grafico1,
-                    // 'grafico2' => $grafico2,
+                    'grafico2' => $grafico2,
                     // 'grafico3' => $grafico3,
                     // 'grafico4' => $grafico4,
                     // 'grafico5' => $grafico5,
@@ -108,4 +109,33 @@ class ReportesReparacionesController extends Controller
      * Este método obtiene la cantidad de mantenciones realizadas por cada categoría de reparación. (MANTENCION CORRECTIVA, MANTENCION PREVENTIVA)
      *
      */
+    public function Grafico2(Request $request)
+    {
+        try{
+            // Obtener los datos opcionales de la request (fechas de inicio y fin) y la OFICINA_ID del usuario logueado
+            $fechaInicio = $request->input('fecha_inicio');
+            $fechaFin = $request->input('fecha_fin');
+            $oficinaId = Auth::user()->OFICINA_ID; // Obtener el ID de la oficina del usuario autenticado
+
+            // Categorías específicas a considerar
+            $categorias = ['MANTENCION CORRECTIVA', 'MANTENCION PREVENTIVA'];
+
+            // Filtrar solicitudes de reparación por categoría y unir con categorías para obtener el nombre
+            $mantencionesPorCategoria = SolicitudReparacion::join('categorias_reparaciones', 'solicitudes_reparaciones.CATEGORIA_REPARACION_ID', '=', 'categorias_reparaciones.CATEGORIA_REPARACION_ID')
+                ->whereIn('categorias_reparaciones.CATEGORIA_REPARACION_NOMBRE', $categorias)
+                ->groupBy('categorias_reparaciones.CATEGORIA_REPARACION_NOMBRE')
+                ->select('categorias_reparaciones.CATEGORIA_REPARACION_NOMBRE', DB::raw('COUNT(solicitudes_reparaciones.SOLICITUD_REPARACION_ID) as cantidad'))
+                ->get();
+
+            // Devolver la cantidad de reparaciones por categoria junto con el nombre de la categoría
+            return [
+                'mantencionesPorCategoria' => $mantencionesPorCategoria,
+            ];
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error al obtener la cantidad de reparaciones por categoria: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
 }
