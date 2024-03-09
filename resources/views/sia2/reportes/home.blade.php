010f19e95cdae88c1778ed6a88b48d9d09c8cccf
FranciscoTarroW11 <franciscomunoz142@gmail.com>
2024-03-09 20:13:34 -0300

    acceso por roles graficos (Backend,frontend,kernel->registra "role" como alias para middlewares)

--- resources/views/sia2/reportes/home.blade.php
+++ resources/views/sia2/reportes/home.blade.php
@@ -7,6 +7,7 @@
 @stop
 
 @section('content')
+    {{-- Mensajes de alerta --}}
     @if (session('success'))
         <script>
             document.addEventListener('DOMContentLoaded', () => {
@@ -30,69 +31,90 @@
             });
         </script>
     @endif
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo de Informes de vehículos</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes de <strong>Vehiculos</strong>. Estadísticas de uso de vehículos, georreferenciación, este módulo permite acceder a la información relacionada con las solicitudes de salida de vehículos.</p>
+
+    {{-- Solo rol administrador o servicios --}}
+    @role('ADMINISTRADOR|SERVICIOS')
+        {{-- Reportes vehiculos --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo de Informes de vehículos</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes de <strong>Vehiculos</strong>. Estadísticas de uso de vehículos, georreferenciación, este módulo permite acceder a la información relacionada con las solicitudes de salida de vehículos.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{route('reportes.vehiculos.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{route('reportes.vehiculos.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+
+        {{-- Reportes materiales --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo de Informes sobre Materiales</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes de <strong>Materiales</strong>. Para saber las cantidades de solicitudes de <strong>Materiales</strong> del sistema completo.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{route('reportes.materiales.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-    </div>
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo de Informes sobre Materiales</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes de <strong>Materiales</strong>. Para saber las cantidades de solicitudes de <strong>Materiales</strong> del sistema completo.</p>
+
+        {{-- Reportes reparaciones --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo de Informes de Reparaciones y Mantenimientos</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes de <strong>reparaciones y mantenciones</strong>. Para saber las cantidades de solicitudes de <strong>reparaciones y mantenciones</strong> del sistema completo.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{route('reportes.reparaciones.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{route('reportes.materiales.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+    @endrole
+
+    {{-- Solo rol ADMINISTRADOR|INFORMATICA --}}
+    @role('ADMINISTRADOR|INFORMATICA')
+        {{-- Reportes equipos --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo de Informes sobre equipos</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes de <strong>Equipos</strong>. Para saber las cantidades de solicitudes de <strong>Equipos</strong> del sistema completo.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{ route('reportes.equipos.index') }}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-    </div>
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo de Informes de Reparaciones y Mantenimientos</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes de <strong>reparaciones y mantenciones</strong>. Para saber las cantidades de solicitudes de <strong>reparaciones y mantenciones</strong> del sistema completo.</p>
+        {{-- Reportes salas --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo de Informes sobre reservas de salas</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes de <strong>Reservas de salas</strong>. Para saber las cantidades de solicitudes de <strong>Reservas de salas</strong> del sistema completo.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{route('reportes.salas.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{route('reportes.reparaciones.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+        {{-- Reportes bodegas --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo de Informes sobre reservas de bodegas</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes de <strong>Reservas de bodegas</strong>. Para saber las cantidades de solicitudes de <strong>Reservas de bodegas</strong> del sistema completo.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{route('reportes.bodegas.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-    </div>
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo de Informes sobre equipos</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes de <strong>Equipos</strong>. Para saber las cantidades de solicitudes de <strong>Equipos</strong> del sistema completo.</p>
+    @endrole
+
+    {{-- Solo rol ADMINISTRADOR --}}
+    @role('ADMINISTRADOR')
+        {{-- Reportes sistema --}}
+        <div class="card text-bg-primary mb-3">
+            <div class="card-header">Módulo Informes del sistema</div>
+            <div class="card-body">
+                <p class="card-text">Este módulo permite ver los reportes del <strong>Sistema</strong>. Para saber las cantidades de solicitudes del <strong>Sistema</strong> del sistema completo.</p>
+            </div>
+            <div class="card-footer">
+                <a class="btn btn-primary" href="{{route('reportes.sistema.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
+            </div>
         </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{ route('reportes.equipos.index') }}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
-        </div>
-    </div>
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo de Informes sobre reservas de salas</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes de <strong>Reservas de salas</strong>. Para saber las cantidades de solicitudes de <strong>Reservas de salas</strong> del sistema completo.</p>
-        </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{route('reportes.salas.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
-        </div>
-    </div>
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo de Informes sobre reservas de bodegas</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes de <strong>Reservas de bodegas</strong>. Para saber las cantidades de solicitudes de <strong>Reservas de bodegas</strong> del sistema completo.</p>
-        </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{route('reportes.bodegas.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
-        </div>
-    </div>
-    <div class="card text-bg-primary mb-3">
-        <div class="card-header">Módulo Informes del sistema</div>
-        <div class="card-body">
-            <p class="card-text">Este módulo permite ver los reportes del <strong>Sistema</strong>. Para saber las cantidades de solicitudes del <strong>Sistema</strong> del sistema completo.</p>
-        </div>
-        <div class="card-footer">
-            <a class="btn btn-primary" href="{{route('reportes.sistema.index')}}"><i class="fa-solid fa-chart-pie"></i> Graficos</a>
-        </div>
-    </div>
+    @endrole
 @stop
 
 @section('css')
--- routes/Reportes/ReportesRoutes.php
+++ routes/Reportes/ReportesRoutes.php
@@ -5,12 +5,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Reportes\ReportesController;
 
 Route::prefix('reportes')->group(function () {
-    Route::get('/home', [ReportesController::class, 'Home'])->name('reportes.home.index');
-    Route::get('/materiales', [ReportesController::class, 'Materiales'])->name('reportes.materiales.index');
-    Route::get('/equipos', [ReportesController::class, 'Equipos'])->name('reportes.equipos.index');
-    Route::get('/salas', [ReportesController::class, 'Salas'])->name('reportes.salas.index');
-    Route::get('/bodegas', [ReportesController::class, 'Bodegas'])->name('reportes.bodegas.index');
-    Route::get('/reparaciones', [ReportesController::class, 'Reparaciones'])->name('reportes.reparaciones.index');
-    Route::get('/sistema', [ReportesController::class, 'Sistema'])->name('reportes.sistema.index');
-    Route::get('/vehiculos', [ReportesController::class, 'Vehiculos'])->name('reportes.vehiculos.index');
+    // Rutas para (ADMINISTRADOR|SERVICIOS)
+    Route::middleware(['role:ADMINISTRADOR|SERVICIOS'])->group(function () {
+        Route::get('/vehiculos', [ReportesController::class, 'Vehiculos'])->name('reportes.vehiculos.index');
+        Route::get('/materiales', [ReportesController::class, 'Materiales'])->name('reportes.materiales.index');
+        Route::get('/reparaciones', [ReportesController::class, 'Reparaciones'])->name('reportes.reparaciones.index');
+    });
+
+    // Rutas para (ADMINISTRADOR|INFORMATICA)
+    Route::middleware(['role:ADMINISTRADOR|INFORMATICA'])->group(function () {
+        Route::get('/equipos', [ReportesController::class, 'Equipos'])->name('reportes.equipos.index');
+        Route::get('/salas', [ReportesController::class, 'Salas'])->name('reportes.salas.index');
+        Route::get('/bodegas', [ReportesController::class, 'Bodegas'])->name('reportes.bodegas.index');
+    });
+
+    // Rutas para (ADMINISTRADOR)
+    Route::middleware(['role:ADMINISTRADOR'])->group(function () {
+        Route::get('/sistema', [ReportesController::class, 'Sistema'])->name('reportes.sistema.index');
+    });
+
+    // Rutas para (ADMINISTRADOR|SERVICIOS|INFORMATICA)
+    Route::middleware(['role:ADMINISTRADOR|SERVICIOS|INFORMATICA'])->group(function () {
+        Route::get('/home', [ReportesController::class, 'Home'])->name('reportes.home.index');
+    });
 });
