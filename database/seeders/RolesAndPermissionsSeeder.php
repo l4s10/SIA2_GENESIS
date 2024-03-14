6dbedf491bad36eef87aa487f8c11047f7029c3a
Francisco munoz <franciscomunoz142@gmail.com>
2024-03-14 20:23:48 -0300

    INCORPORA ROL Y PERMISOS JURIDICO

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -20,6 +20,7 @@ class RolesAndPermissionsSeeder extends Seeder
             'SERVICIOS' => Role::create(['name' => 'SERVICIOS']),
             'INFORMATICA' => Role::create(['name' => 'INFORMATICA']),
             'FUNCIONARIO' => Role::create(['name' => 'FUNCIONARIO']),
+            'JURIDICO' => Role::create(['name' => 'JURIDICO']),
         ];
 
         // Definir permisos (SOLICITUDES)
@@ -47,6 +48,20 @@ class RolesAndPermissionsSeeder extends Seeder
             'actualizar_panel_control',
         ]);
 
+        // Definir permisos para (REPOSITORIO)
+        $permisos = array_merge($permisos, [
+            'ver_repositorio',
+            'crear_repositorio',
+            'editar_repositorio',
+            'actualizar_repositorio',
+            'eliminar_repositorio',
+        ]);
+
+        // Definir permisos para (BUSQUEDA RESOLUCIONES)
+        $permisos = array_merge($permisos, [
+            'buscar_resoluciones',
+        ]);
+
         // Crear permisos
         foreach ($permisos as $permiso) {
             Permission::create(['name' => $permiso]);
@@ -57,14 +72,27 @@ class RolesAndPermissionsSeeder extends Seeder
 
         // Asignación de permisos (SOLICITUDES)
         $roles['FUNCIONARIO']->givePermissionTo(['crear_solicitud', 'ver_solicitudes']);
+
+        $roles['JURIDICO']->givePermissionTo(['crear_solicitud', 'ver_solicitudes']);
+
         $roles['SERVICIOS']->givePermissionTo(['crear_solicitud', 'ver_solicitudes', 'editar_solicitud', 'actualizar_solicitud']);
+
         $roles['INFORMATICA']->givePermissionTo(['crear_solicitud', 'ver_solicitudes', 'editar_solicitud', 'actualizar_solicitud']);
 
         // Asignación de permisos (ACTIVOS - INVENTARIO)
         $roles['SERVICIOS']->givePermissionTo(['crear_activo', 'ver_activos', 'editar_activo', 'actualizar_activo']);
         $roles['INFORMATICA']->givePermissionTo(['crear_activo', 'ver_activos', 'editar_activo', 'actualizar_activo']);
 
+        // Asignación de permisos (PANEL DE CONTROL)
         $roles['SERVICIOS']->givePermissionTo(['ver_panel_control', 'editar_panel_control', 'actualizar_panel_control']);
         $roles['INFORMATICA']->givePermissionTo(['ver_panel_control', 'editar_panel_control', 'actualizar_panel_control']);
+
+        // Asignación de permisos (REPOSITORIO - JURIDICO)
+        $roles['JURIDICO']->givePermissionTo(['ver_repositorio', 'crear_repositorio', 'editar_repositorio', 'actualizar_repositorio', 'eliminar_repositorio']);
+
+        // Asignación de permisos (BUSQUEDA RESOLUCIONES)
+        $roles['JURIDICO']->givePermissionTo(['buscar_resoluciones']);
+        $roles['SERVICIOS']->givePermissionTo(['buscar_resoluciones']);
+        $roles['INFORMATICA']->givePermissionTo(['buscar_resoluciones']);
     }
 }
