8a8459cad2b1497e17db0b520d885f1a0d44ad25
Francisco munoz <franciscomunoz142@gmail.com>
2024-03-15 00:18:54 -0300

    validacion backend JURIDICOS

--- routes/Directivos/FacultadesRoutes.php
+++ routes/Directivos/FacultadesRoutes.php
@@ -7,32 +7,32 @@ use App\Http\Controllers\Directivos\FacultadController;
 Route::prefix('facultades')->group(function () {
     //** Listar facultades */
     Route::get('/', [FacultadController::class, 'index'])
-        ->name('facultades.index');
-        //->middleware('can:ver_facultades');
+        ->name('facultades.index')
+        ->middleware('can:ver_repositorio');
 
     //  Crear Facultad
     Route::get('/facultades/create', [FacultadController::class, 'create'])
-        ->name('facultades.create');
-        //->middleware('can:crear_facultad');
+        ->name('facultades.create')
+        ->middleware('can:crear_repositorio');
 
-    //  Guardar facultad 
+    //  Guardar facultad
     Route::post('/', [FacultadController::class, 'store'])
-        ->name('facultades.store');
-        //->middleware('can:crear_facultad');
+        ->name('facultades.store')
+        ->middleware('can:crear_repositorio');
 
-    
+
     //  Editar facultad
     Route::get('/{facultad}/edit', [FacultadController::class, 'edit'])
-        ->name('facultades.edit');
-        //->middleware('can:editar_facultad');
+        ->name('facultades.edit')
+        ->middleware('can:editar_repositorio');
 
     //  Actualizar facultad
     Route::put('/{facultad}', [FacultadController::class, 'update'])
-        ->name('facultades.update');
-        //->middleware('can:actualizar_facultad');
+        ->name('facultades.update')
+        ->middleware('can:actualizar_repositorio');
 
     //  Eliminar facultad
     Route::delete('/{facultad}', [FacultadController::class, 'destroy'])
-        ->name('facultades.destroy');
-        //->middleware('can:eliminar_facultad');
+        ->name('facultades.destroy')
+        ->middleware('can:eliminar_repositorio');
 });
--- routes/Directivos/PolizasRoutes.php
+++ routes/Directivos/PolizasRoutes.php
@@ -7,32 +7,32 @@ use App\Http\Controllers\Directivos\PolizaController;
 Route::prefix('polizas')->group(function () {
     //** Listar polizas */
     Route::get('/', [PolizaController::class, 'index'])
-        ->name('polizas.index');
-        //->middleware('can:ver_polizas');
+        ->name('polizas.index')
+        ->middleware('can:ver_repositorio');
 
     //  Crear poliza
     Route::get('/polizas/create', [PolizaController::class, 'create'])
-        ->name('polizas.create');
-        //->middleware('can:crear_poliza');
+        ->name('polizas.create')
+        ->middleware('can:crear_repositorio');
 
-    //  Guardar poliza 
+    //  Guardar poliza
     Route::post('/', [PolizaController::class, 'store'])
-        ->name('polizas.store');
-        //->middleware('can:crear_poliza');
+        ->name('polizas.store')
+        ->middleware('can:crear_repositorio');
 
-    
+
     //  Editar poliza
     Route::get('/{poliza}/edit', [PolizaController::class, 'edit'])
-        ->name('polizas.edit');
-        //->middleware('can:editar_poliza');
+        ->name('polizas.edit')
+        ->middleware('can:editar_repositorio');
 
     //  Actualizar poliza
     Route::put('/{poliza}', [PolizaController::class, 'update'])
-        ->name('polizas.update');
-        //->middleware('can:actualizar_poliza');
+        ->name('polizas.update')
+        ->middleware('can:actualizar_repositorio');
 
     //  Eliminar poliza
     Route::delete('/{poliza}', [PolizaController::class, 'destroy'])
-        ->name('polizas.destroy');
-        //->middleware('can:eliminar_poliza');
+        ->name('polizas.destroy')
+        ->middleware('can:eliminar_repositorio');
 });
